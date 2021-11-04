6b2c3937328a9f1a4eee14440665c14eeb26b164
Maksim Uskov <MUskov87@mail.ru>
2021-11-05 02:22:21 +0400

    Initial Commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,81 @@
+
+//horo  = args [0] - знак гороскопа
+
+public class Application {
+    public static void main(String[] args) {
+        String horo  = args [0];
+        if(args[0].equals("Овен")) {
+
+            System.out.println("Интуиция подсказывает мне, что Ваш знак зодиака - Овен");
+        }
+
+        if(args[0].equals("Телец"))
+
+        {
+            System.out.println("Я случайно узнал, что Вы Телец");
+        }
+
+        if(args[0].equals("Близнецы"))
+
+        {
+            System.out.println("Нет лучшего знка зодиака, чем близнецы");
+        }
+
+        if(args[0].equals("Раки"))
+
+        {
+            System.out.println("Уверен, что вы Рак");
+        }
+
+        if(args[0].equals("Лев"))
+
+        {
+            System.out.println("Сразу видно, что Вы царь зверей");
+        }
+
+        if(args[0].equals("Дева"))
+
+        {
+            System.out.println("Дева, это отличный знак зодиака");
+        }
+
+        if(args[0].equals("Весы"))
+
+        {
+            System.out.println("Весы, это отличный знак зодиака");
+        }
+
+        if(args[0].equals("Скорпион"))
+
+        {
+            System.out.println("Я случайно узнал, что Вы Скорпион");
+        }
+
+        if(args[0].equals("Стрелец"))
+
+        {
+            System.out.println("Не зря Вы стрелец по гороскопу, с меткостью у Вас проблем нет");
+        }
+
+        if(args[0].equals("Козерог"))
+
+        {
+            System.out.println("Слышал, что Козероги очень щедрые");
+        }
+
+        if(args[0].equals("Водолей"))
+
+        {
+            System.out.println("Я случайно узнал, что Вы Водолей");
+        }
+
+        if(args[0].equals("Рыба"))
+
+        {
+            System.out.println("Рыба, это отличный знак зодиака");
+        }
+else
+            System.out.println("Вы слишком много выпили, вспоминайте свой знак зодиака");
+    }
+
+}
